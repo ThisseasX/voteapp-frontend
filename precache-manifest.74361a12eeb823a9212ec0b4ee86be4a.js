aebff63d63defbf9c1f25500aa98458141225d3a
Thisseas X <thisseasx@yahoo.com>
2019-06-25 02:23:54 +0300

    Updates

---
+++ precache-manifest.74361a12eeb823a9212ec0b4ee86be4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71ed46fd5e539a935a81db8a8c9b5795",
+    "url": "/voteapp-frontend/static/media/bg.71ed46fd.jpg"
+  },
+  {
+    "revision": "70be80b85f1450dcbcb0",
+    "url": "/voteapp-frontend/static/js/runtime~main.17966dca.js"
+  },
+  {
+    "revision": "ab44dbbe96fb25d43e30",
+    "url": "/voteapp-frontend/static/js/main.1c98dbc5.chunk.js"
+  },
+  {
+    "revision": "529f14dcaa2c34d4b104",
+    "url": "/voteapp-frontend/static/js/2.b59f91bd.chunk.js"
+  },
+  {
+    "revision": "ab44dbbe96fb25d43e30",
+    "url": "/voteapp-frontend/static/css/main.538c6ace.chunk.css"
+  },
+  {
+    "revision": "529f14dcaa2c34d4b104",
+    "url": "/voteapp-frontend/static/css/2.399966e5.chunk.css"
+  },
+  {
+    "revision": "da1118459a1a3259f474b781dfb32f2e",
+    "url": "/voteapp-frontend/index.html"
+  }
+];
